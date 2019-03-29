047c42ecd6f5d0a09a244340767656bc2c83b4a8
Ira Selmon <ira.selmon@opusinspection.com>
2019-03-29 10:11:25 -0700

    refactor: Changed restart to do a different analysis.

--- bikeshare.py
+++ bikeshare.py
@@ -238,7 +238,7 @@ def main():
                 data_loop_count += 1
                 show_data = input('\nWould you like to see some more raw data? Enter yes or no.\n')
 
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        restart = input('\nWould you like to do a different analysis? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
 
